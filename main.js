a0e72419ad57a4832784ceb226ef69eca0f4d83c
LYANNA BUENO <anabuengomes@hotmail.com>
2023-11-12 01:45:20 -0300

    inicial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true
+            }
+        },
+
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira seu email',
+            telefone: 'Por favor, insira seu telefone',
+            mensagem: 'Por favor, insira sua dúvida ou mensagem'
+        },
+
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
